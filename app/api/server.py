6c136a5287e4eff5a12bb49832176bb7f9dce588
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-03-24 11:44:54 +0300

    Refactored

--- app/api/server.py
+++ app/api/server.py
@@ -13,13 +13,13 @@ class Server:
     def __init__(self, host, port, default_media_path):
         self.host = host
         self.port = port
-        self.media_urls = {i: default_media_path for i in range(1, 7)}
+        self.media_paths = {i: default_media_path for i in range(1, 7)}
 
         self.app = Flask(__name__)
         self.app.add_url_rule('/', view_func=self.get_home)
         self.app.add_url_rule('/home', view_func=self.get_home)
-        self.app.add_url_rule('/screen/<number>', view_func=self.get_media_url)
-        self.app.add_url_rule('/screen/<number>', view_func=self.update_media_url, methods=['PUT'])
+        self.app.add_url_rule('/screen/<number>', view_func=self.get_media_path)
+        self.app.add_url_rule('/screen/<number>', view_func=self.update_media_path, methods=['PUT'])
         self.app.add_url_rule('/shutdown', view_func=self.shutdown)
 
         self.app.register_error_handler(404, self.page_not_found)
@@ -43,24 +43,24 @@ class Server:
     def page_not_found(self, error_description):
         return jsonify(error=str(error_description)), 404
 
-    def get_media_url(self, number):
+    def get_media_path(self, number):
         number = int(number)
         if type(number) == int and 1 <= number <= 6:
             try:
-                return send_file(self.media_urls[number],
+                return send_file(self.media_paths[number],
                                  attachment_filename='python.jpg')
             except Exception as e:
                 return str(e)
         else:
             abort(404, description='Screen number is int between 1 and 6.')
 
-    def update_media_url(self, number):
+    def update_media_path(self, number):
         number = int(number)
         if type(number) == int and 1 <= number <= 6:
             request_body = dict(request.json)
-            new_url = request_body.get('url', None)
-            self.media_urls[number] = new_url
-            return f'Successfully updated url for screen {number}', 200
+            new_path = request_body.get('path', None)
+            self.media_paths[number] = new_path
+            return f'Successfully updated media path for screen {number}', 200
         else:
             abort(404, description='Screen number is int between 1 and 6.')
 
@@ -88,6 +88,6 @@ if __name__ == '__main__':
     route = 'screen/1'
     response = requests.put(f"{protocol}://{server_host}:{server_port}/{route}",
                             json={
-                                "url": "/home/al/.local/src/repos/vk-backend/kostya.jpeg"
+                                "path": "/home/al/.local/src/repos/vk-backend/kostya.jpeg"
                             })
     print(response.text)
